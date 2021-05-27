62dd335c2b146e7c32ef8a36500e91bd89da09b9
Jools Barnett <vitamins999@gmail.com>
2021-05-27 11:56:20 +0100

    added favicon

--- components/layout.js
+++ components/layout.js
@@ -5,6 +5,18 @@ const Layout = ({ children, title }) => {
     <>
       <Head>
         <title>Watchlist | {title}</title>
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/icons/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/icons/favicon-16x16.png'
+        />
       </Head>
       <div>{children}</div>
     </>
