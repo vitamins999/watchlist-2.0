04bf8105666b7a349da924ba09b413aa3c39b4a1
Jools Barnett <vitamins999@gmail.com>
2021-06-09 11:31:11 +0100

    added and styled footer component

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Header = ({ listName, region }) => {
+const Header = () => {
   return (
-    <header className='shadow-sm flex justify-between items-center w-full py-5 px-80 bg-gray-200'>
+    <header className='flex justify-between items-center w-full py-5 px-80 bg-gray-200'>
       <Link href='/'>
         <a className='flex'>
           <Image src='/icons/icon.webp' width={36} height={32} />
